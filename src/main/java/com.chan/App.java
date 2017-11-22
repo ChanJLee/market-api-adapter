1f63c69fca081966766c5162d2d89f4f59c5da8c
ChanJLee <1355425625@qq.com>
2017-11-22 22:07:59 +0800

    push code

--- src/main/java/com.chan/App.java
+++ src/main/java/com.chan/App.java
@@ -4,7 +4,6 @@ import com.chan.api.MarketApi;
 import com.chan.api.binance.BinanceMarketApi;
 import com.chan.api.gate.GateMarketApi;
 import com.chan.api.huobi.HuoBiMarketApi;
-import com.chan.common.log.Logger;
 import com.chan.common.preference.Preference;
 import com.chan.model.Ticker;
 import com.chan.model.Type;
@@ -13,24 +12,18 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by chan on 2017/11/21.
+ * Created by chan on 2017/11/22.
  */
 public class App {
     public static void main(String[] args) {
-
         if (args == null || args.length == 0) {
             return;
         }
 
-        File config = new File(args[0]);
-        Preference preference = Preference.getInstance(config);
-
-        Logger.log("huo bi key", preference.getString("HUO_BI_ACCESS_KEY"));
-        Logger.log("huo bi secret", preference.getString("HUO_BI_SECRET_KEY"));
+        Preference preference = Preference.getInstance(new File(args[0]));
 
         MarketApi huoBiMarketApi = new HuoBiMarketApi(preference.getString("HUO_BI_ACCESS_KEY"),
                 preference.getString("HUO_BI_SECRET_KEY"));
-
         MarketApi gateMarketApi = new GateMarketApi("8299346f-17ee81c3-8b7fa980-1f1d3",
                 "15342e59-a8bfd7b7-aa124530-407ff");
         MarketApi binanceMarketApi = new BinanceMarketApi("8299346f-17ee81c3-8b7fa980-1f1d3",
