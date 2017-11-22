136f53cb31a67ad9c56b1ea80a9f00b1fb1e44b4
ChanJLee <1355425625@qq.com>
2017-11-22 21:55:38 +0800

    push code

---
+++ fuck.sh
@@ -0,0 +1,6 @@
+# /bin/bash
+
+git pull origin master
+git add .
+git commit -m "push code"
+git push origin master
---
+++ src/main/java/com.chan/App.java
@@ -0,0 +1,52 @@
+package com.chan;
+
+import com.chan.api.MarketApi;
+import com.chan.api.binance.BinanceMarketApi;
+import com.chan.api.gate.GateMarketApi;
+import com.chan.api.huobi.HuoBiMarketApi;
+import com.chan.common.log.Logger;
+import com.chan.common.preference.Preference;
+import com.chan.model.Ticker;
+import com.chan.model.Type;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by chan on 2017/11/21.
+ */
+public class App {
+    public static void main(String[] args) {
+
+        if (args == null || args.length == 0) {
+            return;
+        }
+
+        File config = new File(args[0]);
+        Preference preference = Preference.getInstance(config);
+
+        Logger.log("huo bi key", preference.getString("HUO_BI_ACCESS_KEY"));
+        Logger.log("huo bi secret", preference.getString("HUO_BI_SECRET_KEY"));
+
+        MarketApi huoBiMarketApi = new HuoBiMarketApi(preference.getString("HUO_BI_ACCESS_KEY"),
+                preference.getString("HUO_BI_SECRET_KEY"));
+
+        MarketApi gateMarketApi = new GateMarketApi("8299346f-17ee81c3-8b7fa980-1f1d3",
+                "15342e59-a8bfd7b7-aa124530-407ff");
+        MarketApi binanceMarketApi = new BinanceMarketApi("8299346f-17ee81c3-8b7fa980-1f1d3",
+                "15342e59-a8bfd7b7-aa124530-407ff");
+
+        try {
+            Ticker ticker = huoBiMarketApi.fetchTicker(Type.ETH_USDT);
+            System.out.println("huo bi ticker: buy " + ticker.buy + " sell " + ticker.sell);
+
+            ticker = gateMarketApi.fetchTicker(Type.ETH_USDT);
+            System.out.println("gate ticker: buy " + ticker.buy + " sell " + ticker.sell);
+
+            ticker = binanceMarketApi.fetchTicker(Type.ETH_USDT);
+            System.out.println("binance ticker: buy " + ticker.buy + " sell " + ticker.sell);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
